e4c4dbbb50d7beb92c6ac66824db7b2c7d66b0a2
Femnir81 <inmydefence81@yahoo.it>
2022-11-14 01:31:42 +0100

    Update post-details.page.ts

--- src/app/pages/post-details.page.ts
+++ src/app/pages/post-details.page.ts
@@ -6,21 +6,36 @@ import { PostsService } from '../posts.service';
 
 @Component({
   template: `
-    <div class="container ">
+    <div class="container my-5">
       <ng-container *ngIf="post; else elseTemplate">
-        <h1 class="text-center mt-4 mb-3">
+        <div class="card mb-4">
+          <h1 class="text-center mt-4 mb-3">
           {{ post.title }}
-        </h1>
-        <p>{{post.body}}</p>
-        <p>Categoria: {{post.type}}</p>
-        <p>Autore: {{post.author}}</p>
+          </h1>
+          <div class="card-body">
+            <p>{{post.body}}</p>
+            <p><span>Categoria:</span> {{post.type}}</p>
+            <p><span>Autore:</span> {{post.author}}</p>
+          </div>
+        </div>
       </ng-container>
     </div>
     <ng-template #elseTemplate>
       <h1 class="text-center mt-5">post non trovato</h1>
     </ng-template>
   `,
-  styles: [],
+  styles: [`
+  .container {
+    max-width: 800px;
+  }
+  .card {
+    background-color: skyblue;
+    box-shadow: 5px 5px 15px 3px black;    
+  }
+  span {
+    font-weight: 500;
+  }
+  `],
 })
 export class PostDetailsPage implements OnInit {
   sub!: Subscription;
